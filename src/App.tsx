19c4f8b9d783d5e034e0f1cff8fc48670f48b84d
jarno <jarno.fret@student.kdg.be>
2024-11-15 17:10:06 +0100

    added Dockerfile and GamePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import { HomePage } from './pages/homePage/HomePage'
+import { GamePage } from './pages/gamePage/GamePage'
 
 function App() {
 
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/Game' element={<GamePage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/gamePage/GamePage.tsx
@@ -0,0 +1,12 @@
+
+
+
+export function GamePage() {
+  return (
+    <div>
+      <h1>tijd om te kassen!!!</h1>
+      
+    </div>
+  )
+}
+
