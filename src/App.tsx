f3a5d613fa640aadc899b929ff79eb7265fde41b
Yannick Vandenbulcke <Yannick.Vandenbulcke@student.kdg.be>
2025-01-05 21:33:25 +0100

    Tried fixing the last thing

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import './App.css'
-import { HomePage } from './pages/HomePage'
-import { GamePage } from './pages/GamePage'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import {HomePage} from './pages/HomePage'
+import {GamePage} from './pages/GamePage'
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
+import {ReactQueryDevtools} from '@tanstack/react-query-devtools'
 import SecurityContextProvider from "./context/SecurityContextProvider.tsx";
 import axios from 'axios'
-import { LobbyPage } from "./pages/LobbyPage.tsx";
-import { AchievementsPage } from "./pages/AchievementsPage.tsx";
-import { SettingsPage } from "./pages/SettingsPage.tsx";
-import { GameSelectorPage } from "./pages/GameSelectorPage.tsx";
-import { Background } from './components/background/Background.tsx'
-import { RouteGuard } from './components/RouteGuard.tsx'
-import { LobbyCodePage } from './pages/LobbyCodePage.tsx'
+import {LobbyPage} from "./pages/LobbyPage.tsx";
+import {AchievementsPage} from "./pages/AchievementsPage.tsx";
+import {SettingsPage} from "./pages/SettingsPage.tsx";
+import {GameSelectorPage} from "./pages/GameSelectorPage.tsx";
+import {Background} from './components/background/Background.tsx'
+import {RouteGuard} from './components/RouteGuard.tsx'
+import {LobbyCodePage} from './pages/LobbyCodePage.tsx'
 import UserprofilePage from './pages/UserprofilePage.tsx'
 import FriendListPage from "./pages/FriendListPage.tsx";
 import ShopPage from "./pages/ShopPage.tsx";
@@ -24,22 +24,22 @@ const queryClient = new QueryClient();
 
 
 function App() {
-    const { backgroundColor } = useSettings();
+    const {backgroundColor} = useSettings();
     return (
         <QueryClientProvider client={queryClient}>
             <SecurityContextProvider>
                 <BrowserRouter>
-                    <ReactQueryDevtools initialIsOpen={false} />
-                    <div className='bg-neutral-900  w-screen h-screen'>
-                        <Background color={`${backgroundColor}`} />
+                    <ReactQueryDevtools initialIsOpen={false}/>
+                    <div className='bg-neutral-900 w-screen h-screen'>
+                        <Background color={`${backgroundColor}`}/>
                         <Routes>
-                            <Route path='/' element={<HomePage />} />
-                            <Route path='/Game' element={<RouteGuard><GamePage /></RouteGuard>} />
-                            <Route path='/Achievements' element={<RouteGuard><AchievementsPage /></RouteGuard>} />
-                            <Route path='/Settings' element={<RouteGuard><SettingsPage /></RouteGuard>} />
-                            <Route path='/GameSelectorPage' element={<RouteGuard><GameSelectorPage /></RouteGuard>} />
-                            <Route path='/LobbyCodePage' element={<RouteGuard><LobbyCodePage /></RouteGuard>} />
-                            <Route path='/Lobby' element={<RouteGuard><LobbyPage /></RouteGuard>} />
+                            <Route path='/' element={<HomePage/>}/>
+                            <Route path='/Game' element={<RouteGuard><GamePage/></RouteGuard>}/>
+                            <Route path='/Achievements' element={<RouteGuard><AchievementsPage/></RouteGuard>}/>
+                            <Route path='/Settings' element={<RouteGuard><SettingsPage/></RouteGuard>}/>
+                            <Route path='/GameSelectorPage' element={<RouteGuard><GameSelectorPage/></RouteGuard>}/>
+                            <Route path='/LobbyCodePage' element={<RouteGuard><LobbyCodePage/></RouteGuard>}/>
+                            <Route path='/Lobby' element={<RouteGuard><LobbyPage/></RouteGuard>}/>
                             <Route path='/Userprofile/:userId' element={<RouteGuard><UserprofilePage/></RouteGuard>}/>
                             <Route path='/FriendList/:userId' element={<RouteGuard><FriendListPage/></RouteGuard>}/>
                             <Route path='/shop' element={<RouteGuard><ShopPage/></RouteGuard>}/>
--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -6,32 +6,47 @@ import SecurityContext from "../context/SecurityContext.ts";
 import useUsers from "../hooks/useUsers.ts";
 import {NotificationCard} from "../components/notifications/notificationCard/NotificationCard.tsx";
 import {NotificationType} from "../models/Notification.ts";
+import useSettings from "../hooks/useSettings.ts";
 
 export function SettingsPage() {
     const navigate = useNavigate();
     const {loggedUserId} = useContext(SecurityContext);
+    const {handleBackgroundColorChange, handleUserAvatarColorChange} = useSettings();
     const {userCustomizables, isLoading, isError} = useUsers(loggedUserId);
-    const backgroundSettings = userCustomizables?.find(c => c.name.includes('Background'));
-    const profilePictureBackgroundSettings = userCustomizables?.find(c => c.name.includes('Profile_Background'));
-
-    const [backgroundColor, setBackgroundColor] = useState('#ffffff');
-    const [profilePictureBackground, setProfilePictureBackground] = useState('#ffffff');
+    const [selectedBackground, setSelectedBackground] = useState('Default white');
+    const [selectedProfileBackground, setSelectedProfileBackground] = useState('Default white');
 
     useEffect(() => {
+        const backgroundSettings = userCustomizables?.find(c =>  !c.name.includes('Profile'));
+        const profilePictureBackgroundSettings = userCustomizables?.find(c => c.name.includes('Profile_Background'));
+
         if (backgroundSettings) {
-            setBackgroundColor(backgroundSettings.color);
+            handleBackgroundColorChange(backgroundSettings.color);
         }
         if (profilePictureBackgroundSettings) {
-            setProfilePictureBackground(profilePictureBackgroundSettings.color);
+            setSelectedProfileBackground(profilePictureBackgroundSettings.name);
+            handleUserAvatarColorChange(profilePictureBackgroundSettings.color);
         }
-    }, [backgroundSettings, profilePictureBackgroundSettings]);
+    }, [userCustomizables]);
 
-    const handleSetBackground = (color: string) => {
-        setBackgroundColor(color);
+    const handleBackgroundChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        const selectedName = event.target.value;
+        const selectedColor = userCustomizables.find(c => c.name === selectedName)?.color;
+        if (selectedColor) {
+            setSelectedBackground(selectedName);
+            console.log("change color to: ",selectedColor);
+            handleBackgroundColorChange(selectedColor);
+        }
     };
 
-    const handleSetProfilePictureBackground = (color: string) => {
-        setProfilePictureBackground(color);
+    const handleProfilePictureBackgroundChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        const selectedName = event.target.value;
+        const selectedColor = userCustomizables.find(c => c.name === selectedName)?.color;
+        if (selectedColor) {
+            console.log("change color to: ",selectedColor);
+            setSelectedProfileBackground(selectedName);
+            handleUserAvatarColorChange(selectedColor);
+        }
     };
 
     if (isLoading || !userCustomizables || isError) {
@@ -46,62 +61,44 @@ export function SettingsPage() {
     }
 
     return (
-        <div className={"bg-gray-500"}>
+        <div>
             <BackButton backAction={() => navigate('/')}/>
             <div className=' absolute top-2 right-2'>
                 <LoginButton/>
             </div>
-            <div className="flex items-center justify-center min-h-screen z-50">
-                <div className="bg-white p-6 rounded-lg shadow-lg">
+            <div className="flex items-center justify-center min-h-screen">
+                <div className="bg-white p-6 rounded-lg shadow-lg z-20">
                     <h1 className="text-3xl font-bold mb-6">Settings</h1>
                     <div className="mb-4">
                         <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="backgroundColor">
                             Background Color
                         </label>
-                        <input
-                            type="color"
+                        <select
                             id="backgroundColor"
-                            value={backgroundColor}
-                            readOnly
+                            value={selectedBackground}
+                            onChange={handleBackgroundChange}
                             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        />
+                        >
+                            {userCustomizables.filter(c => c.name.includes('Background')).map((item) => (
+                                <option key={item.name} value={item.name}>{item.name}</option>
+                            ))}
+                        </select>
                     </div>
                     <div className="mb-4">
                         <label className="block text-gray-700 text-sm font-bold mb-2"
                                htmlFor="profilePictureBackground">
                             Profile Picture Background
                         </label>
-                        <input
-                            type="color"
+                        <select
                             id="profilePictureBackground"
-                            value={profilePictureBackground}
-                            readOnly
+                            value={selectedProfileBackground}
+                            onChange={handleProfilePictureBackgroundChange}
                             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        />
-                    </div>
-                    <div className="mb-4">
-                        <h2 className="text-xl font-bold mb-2">Available Customizables</h2>
-                        <div className="grid grid-cols-2 gap-4">
-                            {userCustomizables.map((item) => (
-                                <button
-                                    key={item.name}
-                                    onClick={() => {
-                                        if (item.name.includes('Background')) {
-                                            handleSetBackground(item.color);
-                                        } else if (item.name.includes('Profile_Background')) {
-                                            handleSetProfilePictureBackground(item.color);
-                                        }
-                                    }}
-                                    className="p-4 border rounded-lg shadow hover:bg-gray-200"
-                                >
-                                    <div className="text-center">
-                                        <div className="text-lg font-bold">{item.name}</div>
-                                        <div className="text-sm">{item.description}</div>
-                                        <div className="mt-2" style={{ backgroundColor: item.color, height: '20px', width: '100%' }}></div>
-                                    </div>
-                                </button>
+                        >
+                            {userCustomizables.filter(c => c.name.includes('Profile_Background')).map((item) => (
+                                <option key={item.name} value={item.name}>{item.name}</option>
                             ))}
-                        </div>
+                        </select>
                     </div>
                 </div>
             </div>
